388ec33af6c0b1d2759d26355634ecafa7600c72
Brad Parker <hi@bradparker.com>
2018-07-26 20:50:17 +1000

    Add check argument for building without tests
    Sounds crazy eh? I wanna be able to build the app without a database
    connection is all.

--- default.nix
+++ default.nix
@@ -1,16 +1,20 @@
 { nixpkgs ? import ./nix/nixpkgs.nix {}
 , compiler ? "default"
+, check ? false
 }:
 let
   packages = if compiler == "default"
     then nixpkgs.haskellPackages
     else nixpkgs.haskell.packages.${compiler};
+  checker = if check
+    then nixpkgs.haskell.lib.doCheck
+    else nixpkgs.haskell.lib.dontCheck;
 in
-  packages.callPackage ./package.nix {
+  checker (packages.callPackage ./package.nix {
     inherit (import ./nix/beam.nix packages {})
       beam-core
       beam-postgres;
 
     inherit (import ./nix/validation.nix packages {})
       validation;
-  }
+  })
