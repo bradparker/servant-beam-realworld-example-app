2f774bc7b2d985568a1a676bf31ffe57e80469f3
Brad Parker <hi@bradparker.com>
2018-07-28 14:18:34 +1000

    Add busybox to docker image, default tag to latest
    For the tag:
    It appears dockerTools doesn't like the sort of tagging that heroku
    requires. So I'm letting it default to "latest" and then using the docker
    cli to tag it following heroku's naming convention.
    
    For the busybox addition:
    Heroku _really_ wants to start the app using `/bin/sh -c` ... so this is
    a stop-gap until I figure out how to make it happy _only_ running the
    app binary.

--- release.nix
+++ release.nix
@@ -1,10 +1,11 @@
 { nixpkgs ? import ./nix/nixpkgs.nix {}
-, tag ? "latest"
 }:
 nixpkgs.dockerTools.buildImage {
   name = "realworld-conduit";
-  tag = tag;
-  contents = import ./. { check = false; };
+  contents = [
+    nixpkgs.busybox
+    (import ./. { check = false; })
+  ];
   config = {
     Cmd = ["/bin/realworld-conduit"];
   };
