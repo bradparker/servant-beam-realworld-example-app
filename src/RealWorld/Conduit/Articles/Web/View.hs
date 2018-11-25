61445fe41af2f519656ce37a7b82b183336f4941
Brad Parker <hi@bradparker.com>
2018-11-25 14:27:20 +1000

    Add optional auth to Articles View
    If we have an auth'd user, then report on whether or not they've fav'd
    an article.

--- src/RealWorld/Conduit/Articles/Web/View.hs
+++ src/RealWorld/Conduit/Articles/Web/View.hs
@@ -7,31 +7,37 @@ module RealWorld.Conduit.Articles.Web.View
 import Control.Monad.Trans.Maybe (MaybeT(MaybeT), maybeToExceptT)
 import Data.Function (($))
 import Data.Functor ((<$>))
-import Data.Maybe (Maybe(Nothing))
 import Data.Text (Text)
 import qualified RealWorld.Conduit.Articles.Database as Database
 import qualified RealWorld.Conduit.Articles.Database.Article as Persisted
 import RealWorld.Conduit.Articles.Web.Article (Article)
 import RealWorld.Conduit.Articles.Web.Create (decorateArticle)
 import RealWorld.Conduit.Handle (Handle(..))
+import RealWorld.Conduit.Users.Web.Claim (Claim)
+import RealWorld.Conduit.Web.Auth (optionallyLoadAuthorizedUser)
 import RealWorld.Conduit.Web.Errors (notFound)
 import RealWorld.Conduit.Web.Namespace (Namespace(Namespace))
 import Servant (Handler(Handler))
 import Servant.API ((:>), Capture, Get, JSON)
+import Servant.Auth.Server (AuthResult(..))
+import Servant.Auth.Swagger (Auth, JWT)
 
 type View =
   "api" :>
   "articles" :>
   Capture "slug" Text :>
+  Auth '[JWT] Claim :>
   Get '[JSON] (Namespace "article" Article)
 
 handler ::
      Handle
   -> Text
+  -> AuthResult Claim
   -> Handler (Namespace "article" Article)
-handler handle slug = do
+handler handle slug authResult = do
+  user <- optionallyLoadAuthorizedUser handle authResult
   article <- loadArticleBySlug handle slug
-  Namespace <$> decorateArticle handle Nothing article
+  Namespace <$> decorateArticle handle user article
 
 loadArticleBySlug :: Handle -> Text -> Handler Persisted.Article
 loadArticleBySlug handle slug =
--- src/RealWorld/Conduit/Web/Auth.hs
+++ src/RealWorld/Conduit/Web/Auth.hs
@@ -1,9 +1,13 @@
 module RealWorld.Conduit.Web.Auth
   ( loadAuthorizedUser
+  , optionallyLoadAuthorizedUser
   ) where
 
+import Control.Applicative (pure)
 import Control.Monad.Trans.Maybe (MaybeT(MaybeT), maybeToExceptT)
 import Data.Function (($))
+import Data.Functor ((<$>))
+import Data.Maybe (Maybe(Just, Nothing))
 import RealWorld.Conduit.Handle (Handle(..))
 import qualified RealWorld.Conduit.Users.Database as Users
 import RealWorld.Conduit.Users.Database.User (PrimaryKey(UserId), User)
@@ -13,9 +17,17 @@ import Servant (Handler(Handler), throwError)
 import Servant.Auth.Server (AuthResult(..))
 
 loadAuthorizedUser :: Handle -> AuthResult Claim -> Handler User
-loadAuthorizedUser handle (Authenticated (Claim id)) =
-  withDatabaseConnection handle $ \conn ->
-    Handler $
-    maybeToExceptT notAuthorized $
-    MaybeT $ Users.find conn (UserId id)
-loadAuthorizedUser _ _ = throwError notAuthorized
+loadAuthorizedUser handle authResult =
+  case authResult of
+    Authenticated (Claim id) ->
+      withDatabaseConnection handle $ \conn ->
+        Handler $
+        maybeToExceptT notAuthorized $
+        MaybeT $ Users.find conn (UserId id)
+    _ -> throwError notAuthorized
+
+optionallyLoadAuthorizedUser :: Handle -> AuthResult Claim -> Handler (Maybe User)
+optionallyLoadAuthorizedUser handle authResult =
+  case authResult of
+    Indefinite -> pure Nothing
+    _ -> Just <$> loadAuthorizedUser handle authResult
